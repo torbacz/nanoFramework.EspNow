c22d2f3fa615f26610b0426479caebddcab5e5d4
José Simões <jose.simoes@eclo.solutions>
2026-07-09 23:19:52 +0100

    Implementing nano pattern for event handler

--- nanoFramework.EspNow/DataReceivedEventArgs.cs
+++ nanoFramework.EspNow/DataReceivedEventArgs.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 // Copyright (c) .NET Foundation and Contributors
 // See LICENSE file in the project root for full license information.
 //
@@ -12,29 +12,30 @@ namespace nanoFramework.EspNow
     /// </summary>
     public class DataReceivedEventArgs : EventArgs
     {
+        private readonly byte[] _peerMac;
+        private readonly byte[] _data;
+        private readonly int _dataLen;
+
+        internal DataReceivedEventArgs(byte[] peerMac, byte[] data, int dataLen)
+        {
+            _peerMac = peerMac;
+            _data = data;
+            _dataLen = dataLen;
+        }
+
         /// <summary>
         /// MAC address of peer data was received from.
         /// </summary>
-        public byte[] PeerMac;
+        public byte[] PeerMac { get => _peerMac; }
 
         /// <summary>
         /// Data received.
         /// </summary>
-        public byte[] Data;
+        public byte[] Data { get => _data; }
 
         /// <summary>
         /// Length of received data.
         /// </summary>
-        public int DataLen;
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        public DataReceivedEventArgs(byte[] peerMac, byte[] data, int dataLen)
-        {
-            this.PeerMac = peerMac;
-            this.Data = data;
-            this.DataLen = dataLen;
-        }
+        public int DataLen { get => _dataLen; }
     }
 }
--- nanoFramework.EspNow/DataSentEventArgs.cs
+++ nanoFramework.EspNow/DataSentEventArgs.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 // Copyright (c) .NET Foundation and Contributors
 // See LICENSE file in the project root for full license information.
 //
@@ -12,24 +12,24 @@ namespace nanoFramework.EspNow
     /// </summary>
     public class DataSentEventArgs : EventArgs
     {
+        private readonly byte[] _peerMac;
+        private readonly int _status;
+
+        internal DataSentEventArgs(byte[] peerMac, int status)
+        {
+            _peerMac = peerMac;
+            _status = status;
+        }
+
         /// <summary>
         /// MAC address of peer data was sent to.
         /// </summary>
-        public byte[] PeerMac;
+        public byte[] PeerMac { get => _peerMac; }
 
         /// <summary>
         /// Status of sending.
         /// See esp_now_send_status_t in esp_now.h
         /// </summary>
-        public int Status;
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        public DataSentEventArgs(byte[] peerMac, int status)
-        {
-            this.PeerMac = peerMac;
-            this.Status = status;
-        }
+        public int Status { get => _status; }
     }
 }
